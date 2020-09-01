b46d4be197c0ff7bfc9d1a74406f3ac56e628aee
AJKappes <alexander.j.kappes@gmail.com>
2020-09-01 15:26:12 -0700

    latent nonlinear gibbs results

--- bayesian_ordinal/bayesian_ord.R
+++ bayesian_ordinal/bayesian_ord.R
@@ -16,25 +16,25 @@ X <- matrix(rnorm(nsamp*p), nrow = nsamp, ncol = p)
 
 # linear latent construction
 y <- 5 + 3*X[, 1] + 4*X[, 1]^2 + rnorm(nsamp)
+ynorm <- (y - min(y))/(max(y) - min(y))
 
 # nonlinear latent construction
-fn_ <- function(X, B = FALSE) {
+L <- 5
+
+# normalize y to (0,1)
+fn_ <- function(X, B) {
   
-  if (B) {
+  if (length(B) > 1) {
     
-    if (length(B) > 1) {
-      
-      fx <- max(y)/(1 + exp(-X%*%B))
-      
-    } else {
-      
-      fx <- max(y)/(1 + exp(-B*X))
-      
-    }
-  
-  } else {
+    fx <- L/(1 + exp(-X%*%B))
     
-    fx <- max(y)/(1 + exp(-X))
+  } else if (is.numeric(B) & length(B) == 1) {
+    
+    fx <- L/(1 + exp(-B*X))  
+    
+  } else if (B == 'cond') {
+    
+    fx <- L/(1 + exp(-X))
     
   }
   
@@ -73,7 +73,7 @@ z <- get_z(y_nl)
 # linear betas
 # betas <- rep(0, p)
 # nonlinear betas
-betas <- rep(0, p)
+betas <- rep(1, p)
 mu0 <- rep(0, p)
 Sigma0 <- diag(length(betas))
 y_std = 1
@@ -82,10 +82,12 @@ u <- c(0, rdirichlet(1, a))
 
 # conditional parameters - fixed
 ## Sigma0 inverse defined by 1/100*Sigma0
+
 ## linear
 # Sigma1 <- solve(1/100*Sigma0 + t(X)%*%X)
+
 ## nonlinear
-Sigma1 <- solve(1/100*Sigma0 + t(fn_(X))%*%fn_(X))
+Sigma1 <- solve(1/100*Sigma0 + t(fn_(X, 'cond'))%*%fn_(X, 'cond'))
 
 ## importance sampling construction
 A <- diag(length(unique(z)))
@@ -102,7 +104,9 @@ A[lower.tri(A)] <- 1
 ##                  mu_1 = 
 ##                  Sigma_1 = 
 ## u | y, betas ~ trunc dir sum_1^{z_i-1}p_k < F(y_i) <= sum_{1}^z_i
-nsim <- 1000
+
+nsim <- 5000
+y_sample <- matrix(0, nrow = nsim, ncol = nsamp)
 U_sample <- matrix(0, nrow = nsim, ncol = length(u))
 betas_sample <- matrix(0, nrow = nsim, ncol = length(betas))
 for (iter in 1:nsim) {
@@ -111,7 +115,7 @@ for (iter in 1:nsim) {
   y_g <- sapply(1:nsamp,
                 function(i) rtnorm(1,
                                    lb = qnorm(sum(u[1:z[i]])),
-                                   ub = qnorm(sum(u[1:z[i] + 1])),
+                                   ub = qnorm(sum(u[1:(z[i] + 1)])),
                                    # linear
                                    # mu = X[i, ]%*%betas,
                                    # nonlinear
@@ -120,10 +124,13 @@ for (iter in 1:nsim) {
   
   # linear weight draw
   ## conditional parameters - update
+  
   ## linear
-  # mu1 <- Sigma1%*%(t(X)%*%y_g + solve(Sigma0)%*%mu0)
-  # nonlinear
-  mu1 <- Sigma1%*%(t(fn_(X))%*%y_g + solve(Sigma0)%*%mu0)
+  # mu1 <- Sigma1%*%(t(X)%*%y_g + 1/100*Sigma0%*%mu0)
+  
+  ## nonlinear
+  mu1 <- Sigma1%*%(t(fn_(X, 'cond'))%*%y_g + 1/100*Sigma0%*%mu0)
+  
   betas <- mvrnorm(mu = mu1, Sigma = Sigma1)
   
   # importance sampling for u draw
@@ -148,58 +155,85 @@ for (iter in 1:nsim) {
   u <- c(0, U_import[, U_draw])
   
   # store draws
+  y_sample[iter, ] <- y_g
   U_sample[iter, ] <- u
   betas_sample[iter, ] <- betas
   
+  cat('Sampling iteration', iter, 'complete',
+      '\n')
+  
 }
- 
+
 
 ### Sampling results ------------------------------------------------
 beta_gibb <- colMeans(betas_sample)[1]
 
 ## linear least squares check
-# beta <- coef(lm(y_g ~ X[, 1] + X[, 1]^2))[2] %>% unname()
+beta <- coef(lm(y_g ~ X[, 1] + X[, 1]^2))[2] %>% unname()
 
 ## nonlinear least squares check
-nlmod <- nls(y_g ~ max(y)/(1 + exp(-b1*X[, 1])),
+y_g_est <- colMeans(y_sample)
+nlmod <- nls(y_g_est ~ L/(1 + exp(-b1*X[, 1])),
              start = list(b1 = 1))
 beta <- coef(nlmod) %>% unname()
-
 
 ## linear approx for nonlinear fn
 # beta <- coef(lm(y_g ~ X[, 1]))[2] %>% unname()
 
 # plot sampling values
-plot(1:nsim, betas_sample[, 1], type = 'l', main = 'Beta_1 Sampling Values')
+# plot(1:nsim, betas_sample[, 1], type = 'l', main = 'Beta_1 Sampling Values')
 
 ## prediction
-y_nl_hat <- fn_(X[, 1], beta_gibb)
-mspe <- 1/nsamp*sum((y_nl - y_nl_hat)^2)
-plot(X[, 1], y_nl, main = 'Prediction Overlay (red)')
-lines(X[, 1], y_nl_hat, type = 'p', col = 'red')
+# linear
+# y_hat <- beta_gibb*X[, 1] + beta_gibb*X[, 1]^2
 
-z_hat <- get_z(y_nl_hat)
-acc_vec <- map2_dbl(z, z_hat,
-                    function(i, ip) ifelse(i == ip, 1, 0))
+# nonlinear
+y_hat <- fn_(X[, 1], beta_gibb)
+# mspe <- 1/nsamp*sum((y_nl - y_hat)^2)
+# plot(X[, 1], y_nl, main = 'Prediction Overlay (red)')
+# lines(X[, 1], y_hat, type = 'p', col = 'red')
+
+u_hat <- colMeans(U_sample)[2:4]
+u_bounds <- sapply(1:(length(u_hat) - 1), function(i) sum(u_hat[1:i]))
+delta <- c(min(y_hat),
+           sapply(1:length(u_bounds), function(i) qnorm(u_bounds[i])))
+obs_bounds <- c(min(y_nl), quantile(y_nl, .33), quantile(y_nl, .67))
+z_hat <- sapply(y_hat,
+                function(z) (z >= delta[1]) + (z > delta[2]) + (z > delta[3])) %>% 
+  unname()
+
+acc_vec <- map2_dbl(z, z_hat, function(i, ip) ifelse(i == ip, 1, 0))
 acc <- mean(acc_vec)
 
 # print results
-cat('Beta_1 Estimates:',
-    '\n (Gibbs sampling)', beta_gibb,
-    '\n (Least squares check)', beta,
-    '\n',
-    '\nOrdinal value prediction accuracy rate:', acc,
-    '\n')
+# cat('Beta_1 Estimates:',
+#     '\n (Gibbs sampling)', beta_gibb,
+#     '\n (Nonlinear least squares check)', beta,
+#     '\n',
+#     '\nDelta Estimates (J = 3):', delta,
+#     '\n',
+#     '\nOrdinal value prediction accuracy rate:', acc,
+#     '\n')
 
 
-### TODO -------------------------------------------------------------
+### Testing -------------------------------------------------------------
 
+## convergence
+beta_conv <- sapply(1:nsim, function(i) mean(betas_sample[1:i, 1]))
 
+u_conv <- matrix(0, nrow = nsim, ncol = length(u_hat))
+for (j in 1:ncol(u_conv)) {
 
+  conv <- sapply(1:nsim, function(i) mean(U_sample[1:i, j + 1]))
+  u_conv[, j] <- conv
 
+}
 
+# par(mfrow = c(2, 2), oma = c(0, 0, 2, 0))
+# plot(1:nsim, beta_conv, main = 'b1')
+# plot(1:nsim, u_conv[, 1], main = 'u1')
+# plot(1:nsim, u_conv[, 2], main = 'u2')
+# plot(1:nsim, u_conv[, 3], main = 'u3')
+# mtext('Sampling Estimator Convergence', outer = TRUE)
 
-
-
-
-
+ 
